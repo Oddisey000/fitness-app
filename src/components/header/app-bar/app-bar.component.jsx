7df4de97dd90090424d1f6cf3e8d6ec3b38f8a4a
Vitalii Pertsovych <oddisey000@gmail.com>
2021-02-12 13:39:05 +0200

    Filtering by muscles group added

--- src/components/header/app-bar/app-bar.component.jsx
+++ src/components/header/app-bar/app-bar.component.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  }
+}));
+
 export default function AppBarComponent() {
+  const classes = useStyles();
   return (
-    <div className="root">
+    <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" className="title">
